223e3acaac6923d244d6b58bf720adf62cb18914
Robert Ferney <capnregex@gmail.com>
2014-01-15 12:28:11 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DnDCity::Application.config.secret_key_base = 'd5f5c79406199e29528635033e4d4e0bfadffd2bcc2c4aae79ee8eecc847ac0a13752b44e074daeb85869a27d9438c442daf038ad237693927368ce0e2ce7a76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DnDCity::Application.config.session_store :cookie_store, key: '_DnDCity_session'
