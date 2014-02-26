4ac212938c71d5bd231912e95d9140dcbe199a3a
Robert Ferney <capnregex@gmail.com>
2014-02-26 14:26:46 -0700

    fixing edit form for character classes

--- app/controllers/character_classes_controller.rb
+++ app/controllers/character_classes_controller.rb
@@ -67,6 +67,8 @@ class CharacterClassesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def character_class_params
-      params.require(:character_class).permit(:name, :desc, :class_abilities, :hit_die, :base_attack_bonus, :base_fort_progression, :base_ref_progression, :base_will_progression, :skill_points_per_level)
+      params.require(:character_class).permit(:name, :desc, :class_abilities,
+      :hit_die, :skill_points_per_level, :bab_type, :fort_type, :ref_type,
+      :will_type)
     end
 end
